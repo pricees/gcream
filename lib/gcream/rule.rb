63e161f48c513efdc4b67f6c5b537ac21cce3c51
Edward Price <ted.price@gmail.com>
2013-10-19 15:02:12 -0500

    modify rules

--- lib/gcream/rule.rb
+++ lib/gcream/rule.rb
@@ -2,7 +2,10 @@ module Gcream
   module Rule
   end
 end
+
+# Load these in order
 require_relative "rule/base.rb"
 require_relative "rule/consecutive.rb"
 
+# Load the rest
 Dir["#{File.dirname(__FILE__)}/rule/*.rb"].each { |file| require file }
